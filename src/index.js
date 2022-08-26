b94d948ef3f0e99672849fb13543427e9b1ee35a
Yevhenii Kolesnichenko <kolesnichenko.yevhenii@pdffiller.team>
2022-08-26 15:55:53 +0300

    [dev] add prettier and ci

--- src/index.js
+++ src/index.js
@@ -9,26 +9,28 @@ const routes = require('./routes/index.route');
 app.use(routes);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    const err = new Error('Not Found!');
-    err.status = 404;
-    next(err);
+app.use(function (req, res, next) {
+  const err = new Error('Not Found!');
+  err.status = 404;
+  next(err);
 });
 
-app.use(function(err, req, res, next) {
-    console.log(err.stack);
+app.use(function (err, req, res, next) {
+  console.log(err.stack);
 
-    res.status(err.status || 500);
+  res.status(err.status || 500);
 
-    res.json({'errors': {
-        message: err.message,
-        error: err
-    }});
+  res.json({
+    errors: {
+      message: err.message,
+      error: err,
+    },
+  });
 });
 
 // start http server
-var server = app.listen( process.env.PORT || 3000, function(){
-    console.log('Listening on port ' + server.address().port);
+var server = app.listen(process.env.PORT || 3000, function () {
+  console.log('Listening on port ' + server.address().port);
 });
 
 module.exports = { app };
