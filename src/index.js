184576296472a50cda612c6ca79a38addd6b3307
Yevhenii Kolesnichenko <kolesnichenko.yevhenii@pdffiller.team>
2022-09-03 22:16:05 +0300

    fix code style

--- src/index.js
+++ src/index.js
@@ -6,10 +6,10 @@ import routes from './routes/indexRoute.js';
 const app = express();
 app.use(cors());
 
-app.get('/test', function (req, res) {
-  console.log(req.query)
-  res.send('Hello! Today is: ' + (new Date().toString()));
-})
+app.get('/test', (req, res) => {
+  console.log(req.query);
+  res.send(`Hello! Today is: ${new Date().toString()}`);
+});
 
 app.use(routes);
 
