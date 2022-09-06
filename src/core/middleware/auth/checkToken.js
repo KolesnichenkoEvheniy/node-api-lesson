74e93c1f0143277cd5579df70619cbe9f87f931c
Yevhenii Kolesnichenko <kolesnichenko.yevhenii@pdffiller.team>
2022-09-06 22:40:34 +0300

    refactor check token impl

--- src/core/middleware/auth/checkToken.js
+++ src/core/middleware/auth/checkToken.js
@@ -2,12 +2,12 @@
 export default (availableTokens) => (req, res, next) => {
     const token = req.query.token ?? false;
 
-    if (token && availableTokens.includes(token)) {
-        return next()
+    if (token && !availableTokens.includes(token)) {
+        let forbidden = new Error('Requests without token are forbidden!');
+        forbidden.status = 403;
+
+        throw forbidden;
     }
 
-    let forbidden = new Error('Requests without token are forbidden!');
-    forbidden.status = 403;
-
-    throw forbidden;
+    next()
 }
